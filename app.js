3bebc570195572e33ecef9206b7ca071efe89fb3
10k_scoop <zeeshankarim9247@gmail.com>
2023-10-22 22:20:31 +0500

    pc

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ import { loadRoutes } from "./services/index.js";
 import { v1Routes } from "./v1RoutesPath/index.js";
 import firebase from "./services/Firebase.js";
 import { Pkey } from "./env/index.js";
-import {setHeaders } from "./middlewares.js";
+import { setHeaders } from "./middlewares.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 4000;
 // load firebase database
 const db = firebase?.db;
 // for parsing application/json
@@ -18,11 +17,14 @@ app.use(setHeaders);
 app.use(express.static("public"));
 
 //Load all routes
-loadRoutes(v1Routes, (path, route) => app.use("/api"+path, route));
+loadRoutes(v1Routes, (path, route) => app.use("/api" + path, route));
 
 //Wrong Route
 app.use("/", async (req, res) => {
-  res.send("<h1>Error 404 Not Found !</h1>");
+  res.send("<h1>Error 404 Not Found !</h1>" + PORT);
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(process.env.PORT);
+  console.log(`Server is running on port ${PORT}`);
+});
